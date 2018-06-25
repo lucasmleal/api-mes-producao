ecda32820160e6b557860d64d123e08583fc9a69
Lucas Manique Leal <lucas@labsit.io>
2018-06-25 00:40:31 -0300

    feat(producao): criados métodos de simulação das fases

---
+++ MesProducao/Controllers/FresaController.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MesProducao.Controllers
+{
+    [Route("api/[controller]")]
+    public class FresaController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var retorno = new List<string>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                retorno.Add("Peça ID " + new Random().Next(1, 9999));
+            }
+
+            return retorno;
+        }
+
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost("iniciar-operacao")]
+        public string IniciarOperacao([FromBody]string value)
+        {
+            return "Iniciada operação de fresa para nova peça. ID: " + new Random().Next(6000, 9999);
+        }
+
+        // PUT api/values/5
+        [HttpPut("finalizar-operacao/{id}")]
+        public string FinalizarOperacao(int id, [FromBody]string value)
+        {
+            return "Finalizada operação de fresa para peça: ID: " + id;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("cancelar-operacao/{id}")]
+        public string Delete(int id)
+        {
+            return "Operação de fresa cancelada para peça: ID: " + id;
+        }
+    }
+}
---
+++ MesProducao/Controllers/SerraController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MesProducao.Controllers
+{
+    [Route("api/[controller]")]
+    public class SerraController : Controller
+    {
+        // GET api/values
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            var retorno = new List<string>();
+
+            for (int i = 0; i < 100; i++)
+            {
+                retorno.Add("Peça ID " + new Random().Next(1, 9999));
+            }
+
+            return retorno;
+        }
+            
+        // GET api/values/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/values
+        [HttpPost("iniciar-operacao")]
+        public string IniciarOperacao([FromBody]string value)
+        {
+            return "Iniciada operação de serra para nova peça. ID: " + new Random().Next(6000, 9999);
+        }
+
+        // PUT api/values/5
+        [HttpPut("finalizar-operacao/{id}")]
+        public string FinalizarOperacao(int id, [FromBody]string value)
+        {
+            return "Finalizada operação de serra para peça: ID: " + id;
+        }
+
+        // DELETE api/values/5
+        [HttpDelete("cancelar-operacao/{id}")]
+        public string Delete(int id)
+        {
+            return "Operação de fresa cancelada para peça: ID: " + id;
+        }
+    }
+}
